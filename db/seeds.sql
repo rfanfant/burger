e023d8d07e0898245602f057ec994553d8d65ed7
Rob Fanfant <rfanfant@hotmail.com>
2019-04-23 21:12:23 -0700

    added database queries

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Kung Foo Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Chocolate Burger", FALSE);
+
+INSERT INTO burgers (burger_name,devoured)
+VALUES ("Strawberry Burger", FALSE);
