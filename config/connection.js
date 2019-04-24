1ee3b757b510e938ed758beffc0e8eead569a2b1
Rob Fanfant <rfanfant@hotmail.com>
2019-04-23 21:44:51 -0700

    Added Heroku env var

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 
 
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'10Blackbird$',
-    database:'burgers_db'
-});
+
+var connection;
+
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    var connection = mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'',
+        database:'burgers_db'
+    });
+};
 
 connection.connect(function(err){
 if(err)throw err;
